51a390a2f22df587abec8be49685a03e7b2056b3
vanhry <vanhry@gmail.com>
2019-05-09 19:03:04 +0300

    Done

--- model.py
+++ model.py
@@ -1,49 +1,34 @@
-import numpy
-from PIL import Image
 import os
-from keras.applications import MobileNet, VGG16
-from keras.layers import Input, MaxPool2D
+from keras.applications import MobileNet
 from keras.callbacks import ReduceLROnPlateau
-from keras.callbacks import ModelCheckpoint
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, Input,  GlobalAveragePooling2D
-from keras.layers import Conv2D, MaxPooling2D, Activation, BatchNormalization
+
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Activation
 from keras.preprocessing.image import ImageDataGenerator
 
 global_path = os.getcwd()
 
 train_dir = 'training'
 test_dir = 'validation'
-my_train = 'my_train'
 IMG_SIZE = 160
 num_classes = 10
 batch_size = 16
 
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
+    rescale=1./255,
     rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
     horizontal_flip=True,
     fill_mode='nearest',
     zoom_range=0.1,
-    # preprocessing_function=brightness_adjustment
     )
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-
-my_train_gen = test_datagen.flow_from_directory(my_train,
-                                                target_size=(IMG_SIZE, IMG_SIZE),
-                                                color_mode='rgb',  # mages will be resized to 150x150
-                                                batch_size=batch_size,
-                                                # ve_to_dir='results',
-                                                # subset='training',
-                                                class_mode='categorical'
-                                                )
 # this is a generator that will read pictures found in
 # subfolers of 'data/train', and indefinitely generate
 # batches of augmented image data
@@ -66,16 +51,14 @@ validation_generator = test_datagen.flow_from_directory(
     class_mode='categorical')
 
 def build_model():
-
-    base_model = MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+    base_model = MobileNet(weights='imagenet',include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(256)(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-    #x = Dropout(0.5)(x)
+    x = Dense(256)(x)
     x = Activation('relu')(x)
     preds = Dense(10,activation='softmax')(x) #final layer with softmax activation
     model=Model(inputs=base_model.input, outputs=preds)
-    for layer in model.layers[:84]: # 87 mobilennet
+    for layer in model.layers[:84]:
         layer.trainable=False
     return model
 
@@ -85,7 +68,7 @@ if __name__ == "__main__":
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
     reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 3, verbose = 1, min_delta = 0.0001)
     callbacks = [reduce_lr]
-    history = model.fit_generator(train_generator, epochs=6, workers=1, callbacks=callbacks,
+    history = model.fit_generator(train_generator, epochs=10, workers=1, callbacks=callbacks,
                                            steps_per_epoch=len(train_generator),
                                            validation_data=validation_generator,
                                            validation_steps=len(validation_generator),
